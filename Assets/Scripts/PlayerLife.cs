4f4dc32013f08bca440f9e3397803654a958d344
Poxie <poxen12@hotmail.com>
2023-07-17 19:07:51 +0200

    Storing and displaying updated death count

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class PlayerLife: MonoBehaviour {
-    int deathCounter = 0;
+    int deathCount;
     bool dead = false;
 
     Rigidbody2D rb;
@@ -13,6 +14,11 @@ public class PlayerLife: MonoBehaviour {
     PlayerController playerController;
     UIManager uiManager;
 
+    // Structure for stats json
+    class Stats {
+        public int deaths;
+    }
+
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -20,6 +26,13 @@ public class PlayerLife: MonoBehaviour {
         animator = transform.Find("Sprite").GetComponent<Animator>();
         animationScript = transform.Find("Sprite").GetComponent<PlayerAnimations>();
         uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+
+        // Fetching death counter from json file
+        using (StreamReader reader = new StreamReader(GetStatsPath())) {
+            string json = reader.ReadToEnd();
+            deathCount = JsonUtility.FromJson<Stats>(json).deaths;
+            uiManager.UpdateDeathCounter(deathCount);
+        }
     }
 
     // Update is called once per frame
@@ -38,12 +51,26 @@ public class PlayerLife: MonoBehaviour {
         }
     }
 
-    void updateDeathCounter() {
-        deathCounter++;
+    string GetStatsPath() {
+        return Application.dataPath + "/Resources/Stats.json";
+    }
+
+    void UpdateDeathCounter() {
+        Stats stats = new Stats();
+        stats.deaths = deathCount + 1;
+        
+        // Updating the UI with new death count
+        uiManager.UpdateDeathCounter(stats.deaths);
+    
+        // Updating stats json
+        FileStream fileStream = new FileStream(GetStatsPath(), FileMode.Create);
+        using (StreamWriter writer = new StreamWriter(fileStream)) {
+            writer.Write(JsonUtility.ToJson(stats));
+        }
     }
 
     public void Die() {
-        updateDeathCounter();
+        UpdateDeathCounter();
         dead = true;
         animationScript.enabled = false;
         animator.Play("Death");
